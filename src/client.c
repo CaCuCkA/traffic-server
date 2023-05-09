1c16b11041fa7b3586601215d181fdb85f071343
Mykola <mykola.yakovkin@ucu.edu.ua>
2023-05-10 02:41:56 +0300

    refactored code in [client.c]

--- src/client.c
+++ src/client.c
@@ -1,42 +1,83 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+#include "utils.h"
 #include "client.h"
 #include "server.h"
 
-#define TRUE 1
-#define BUFFER_SIZE 2048
 
+#define BUFFER_SIZE 2048
+#define COMMAND_SIZE 40
 
 static void
-set_connection_options(int* client_socket, char* socket_path) {
-    create_socket(client_socket, AF_UNIX, NULL);
-
-    struct sockaddr_un server_addr;
-    make_address(&server_addr, AF_UNIX, socket_path);
-
-    connect_socket(client_socket, &server_addr, NULL);
+handle_help_command() {
+    char* commands = "Available commands:\n"
+                     " - start - start sniffing on the specified interface\n"
+                     " - stop - stop sniffing\n"
+                     " - stats [iface] - show statistics for the specified interface\n"
+                     " - show [ip] count - show the number of packets received from the specified IP\n"
+                     " - select [iface] - select specific iface for sniffing\n"
+                     " - help - show this help message\n"
+                     " - exit - exit the program\n";
+    fprintf(stdout, "%s", commands);
 }
 
 static void
-main_loop(int* client_socket) {
+handle_stat_command(char* command, int* client_socket) {
     char buffer[BUFFER_SIZE];
+    send_data_to_socket(client_socket, command, strlen(command), NULL);
+    read_data_from_socket(client_socket, buffer, BUFFER_SIZE, NULL);
+}
 
-    while (TRUE) {
-        fprintf(stdout, "Enter a command (start, stop, status, info, exit): \n");
-        fgets(buffer, sizeof(buffer), stdin);
+static void
+handle_show_command(char* command, int* client_socket) {
+    send_data_to_socket(client_socket, command, strlen(command), NULL);
 
-        send_data_to_socket(client_socket, buffer, strlen(buffer), NULL);
+    int ip_number = 0;
+    read_data_from_socket(client_socket, &ip_number, sizeof(ip_number), NULL);
 
-        memset(buffer, 0, BUFFER_SIZE);
-        read_data_from_socket(client_socket, buffer, BUFFER_SIZE, NULL);
+    strtok(command, " ");
+    char* ip = strtok(NULL, " ");
+    fprintf(stdout, "This IP [%s], has been encountered %d times\n", ip, ip_number);
+}
 
-        if (!strcmp(buffer, "exit")) {
-            break;
+static void
+command_dispatcher(char* command, int* client_socket) {
+    char* token = strtok(strdup(command), " ");
+    size_t command_len = strlen(command);
+
+    if (!strcmp(token, "start") || !strcmp(token, "stop")
+        || !strcmp(token, "select") || !strcmp(token, "exit")) {
+        fprintf(stdout, "%s\n", command);
+        send_data_to_socket(client_socket, command, command_len, NULL);
+        if (token[0] == 'e') {
+            close(*client_socket);
+            exit(0);
         }
+    } else if (!strcmp(token, "stat")) {
+        handle_stat_command(command, client_socket);
+    } else if (!strcmp(token, "show")) {
+        handle_show_command(command, client_socket);
+    } else {
+        handle_help_command();
+    }
+
+}
+
+
+static void
+main_loop(int* client_socket) {
+    char command[COMMAND_SIZE];
+
+    while (true) {
+        memset(command, 0, COMMAND_SIZE);
+        fprintf(stdout, "Enter a command (start, stop, stat, select, show, help, exit): \n");
+        fgets(command, COMMAND_SIZE, stdin);
+        command[strcspn(command, "\n")] = '\0';
+        command_dispatcher(command, client_socket);
     }
 }
 
@@ -44,9 +85,7 @@ void
 run(char* socket_path) {
     int client_socket;
 
-    set_connection_options(&client_socket, socket_path);
+    set_connection_options(&client_socket, socket_path, NULL);
 
     main_loop(&client_socket);
-
-    close(client_socket);
 }
